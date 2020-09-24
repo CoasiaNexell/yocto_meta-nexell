44e55273b20867c820a0982bcbd3792b9a5482eb
darius kim <darius.kim@coasianexell.com>
2020-09-24 13:53:08 +0900

    mata-nexell: s5p6818: add s5p6818 convergence board
    This patch add s5p6818 convergence board support to yocto dunfell
    
    Change-Id: I385bc236abdfa7777192db1f1eac96fef731ed94
    Signed-off-by: darius kim <darius.kim@coasianexell.com>

--- meta-nexell-distro/tools/copyFilesToOutDir.sh
+++ meta-nexell-distro/tools/copyFilesToOutDir.sh
@@ -104,6 +104,8 @@ function copy_bin_files()
                 cp ${TMP_DEPLOY_PATH}/bl1-avn.bin ${RESULT_PATH}
             elif [ "${BOARD_NAME}" == "svt-ref" ]; then
                 cp ${TMP_DEPLOY_PATH}/bl1-svt.bin ${RESULT_PATH}
+            elif [ "${BOARD_NAME}" == "con-svma" ]; then
+                cp ${TMP_DEPLOY_PATH}/bl1-con.bin ${RESULT_PATH}
             fi
             cp ${TMP_DEPLOY_PATH}/fip-loader.bin ${RESULT_PATH}
             cp ${TMP_DEPLOY_PATH}/fip-nonsecure.bin ${RESULT_PATH}
